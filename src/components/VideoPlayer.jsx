04dbd72b895797a17bdcd0517a94313562a97e21
Amaljith M K <amaljithaurify@gmail.com>
2026-06-16 18:26:58 +0530

    Implement deployment workflow and update VideoPlayer component

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 // noo
 const VideoPlayer = () => {
@@ -7,13 +7,13 @@ const VideoPlayer = () => {
       <iframe
         width="100%"
         height="100%"
-        src="https://www.youtube.com/embed/iEpJwprxDdk?autoplay=1&controls=0&showinfo=0&rel=0&modestbranding=1&loop=1&playlist=iEpJwprxDdk&mute=0"
+        src="https://www.youtube.com/embed/xBcugR2fM84?si=tE3LxaB3lCPtlU74"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
-        style={{ minHeight: '440px' }}
-        className='rounded-md'
+        style={{ minHeight: "440px" }}
+        className="rounded-md"
       ></iframe>
     </div>
   );
